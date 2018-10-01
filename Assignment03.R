2f4dbc59756457dd8ac1bc238f7ea65183bba96b
AnduoLyu <anl4002@med.cornell.edu>
2018-10-01 11:43:06 -0400

    Assignment03

---
+++ Assignment03.R
@@ -0,0 +1,23 @@
+library(ggplot2)
+library("gapminder")
+library(dplyr)
+gapminder
+colnames(gapminder)
+gapminder1 <- filter(gapminder, year == 2007)
+annotate <- filter(gapminder1, gdpPercap > 40000)
+p <- ggplot(data = gapminder1, aes(x = gdpPercap, y = lifeExp)) +
+  geom_point() +
+  geom_text(data = annotate, aes(label = country), color = "blue") +
+  labs(x = "GDP per capita", y = "ife expectancy") +
+  theme_bw()
+p
+
+
+pp <- ggplot(data = gapminder1, aes(x = gdpPercap, y = lifeExp)) +
+  geom_point(shape = 1, size = 2, stroke = 1.5) +
+  labs(x = "GDP per capita", y = "ife expectancy") +
+  theme_bw()
+  
+pp + annotate("text", x = 45000 : 44998, y = 73 : 71, label = c("Countries with", "", "highest GDP")) + 
+  annotate("rect", xmin = 38500, xmax = 50000, ymin = 75, ymax = 85, fill = "red", alpha = 0.2)
+
